00a3222762f1d6c5a59eb51b8fd63fd551ab9ae8
__227tn <toshi@n527t.local>
2023-07-26 13:30:48 +0900

    [#4]Implementing Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller? 
+  before_action :configure_permitted_parameters, if: :devise_controller? 
   
-    private
+  private
   
-      def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
-      end
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+    end
+end
   
---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,7 @@
+class HomesController < ApplicationController
+  def index
+  end
+
+  def top
+  end
+end
